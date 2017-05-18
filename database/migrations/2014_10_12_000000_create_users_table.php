6f41e1cb232cdb6250e494ca2f9e8ce6d427f1c5
Simon Blomsterlund <simonblomsterlund@Simons-MacBook-Pro.local>
2017-05-18 20:02:06 +0300

    migrations for user made

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('vacancy');
+            $table->string('driverslicence');
+            $table->string('primary_phone');
+            $table->string('secondary_phone')->nullable();
+            $table->string('telegram_id');
+            $table->string('street_address')->nullable();
+            $table->string('city_address')->nullable();
+            $table->string('postcode_address')->nullable();
+            $table->string('country_address')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('fire_department');
+            $table->boolean('is_admin');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
